bb273ad4ed5cbb4d32ea2fc26143a6c9f4972125
Sheena Bai <xb5@zelda.ece.rice.edu>
2024-07-08 12:17:35 -0500

    videos with bounding box showing

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def login():
         user = User.query.filter_by(username=username).first()
         if user and check_password_hash(user.password, password):
             login_user(user)
-            return redirect(url_for('index'))
+            return redirect(url_for('home'))
         flash('Login Unsuccessful. Please check username and password')
     return render_template('login.html')
 
